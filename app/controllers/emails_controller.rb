32ff67cf9f1cf5491bf6dc37da440141e282d8e0
Jesse Pollak <jpollak92@gmail.com>
2012-06-23 18:34:57 -0400

    removed test

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,7 +2,6 @@ class EmailsController < ApplicationController
   protect_from_forgery :except => :add_email
   
   def add_email
-    p "TEST"
     Email.create email: params[:email], mentor: params[:mentor]
     render nothing: true, status: 200
   end
