4f404f42e9ed003e960e3f032e05539c3d57cfde
Jesse Pollak <jpollak92@gmail.com>
2012-06-23 19:03:49 -0400

    adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -64,5 +64,15 @@ conversations = []
   end
 end
 
-con
-  
+conversations.each do |conversation|
+  (0..9).each do |i|
+    m = conversation.messages.build
+    if i % 2 == 0
+      m.owner_type = "mentor"
+    else
+      m.owner_type = "mentee"
+    end
+    
+    m.value = "Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?"
+  end
+end
