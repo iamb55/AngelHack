cdec87546957c864d3390c8e94f8ebb8e257b2e4
Jordan Goldstein <mr.jordangoldstein@gmail.com>
2012-06-23 21:08:37 -0400

    Add conversation filters for answer status

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,4 +2,16 @@ class Conversation < ActiveRecord::Base
   has_many :messages
   belongs_to :mentee
   belongs_to :mentor
+
+  def self.answered
+    Conversation.all.collect do |conv|
+      conv if conv.messages.count > 1 
+    end.compact
+  end
+
+  def self.unanswered
+    Conversation.all.collect do |conv|
+      conv if conv.messages.count == 1 
+    end.compact
+  end
 end
