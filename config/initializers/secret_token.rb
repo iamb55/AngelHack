016350d37934c9a8561ee7f7f1a9c3abaf83fb7d
Jesse Pollak <jpollak92@gmail.com>
2012-06-24 12:51:32 -0400

    secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-AngelHack::Application.config.secret_token = '378e457171537d39baaeb5ca232d8598496c6ff02a8baddf67a146f0404a14175331b9b27958a9850ec92e9ef61f579de37cc5104147cb8aeccc6501e6af3937'
+AngelHack::Application.config.secret_token = '378e457171537d39baaeb5ca2d8598496c6ff02a8baddf67a146f0404a14175331b9b27958a9850ec92e9ef61f579de37cc5104147cb8aeccc6501e6af3937'
