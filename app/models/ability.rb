3efa980238ad5d022cacee3692c4d6a1bcefa1cf
Jordan Goldstein <mr.jordangoldstein@gmail.com>
2012-06-24 09:19:59 -0400

    Authorize for mentees

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,6 +9,7 @@ class Ability
     elsif user.user_type == "mentee"
       can :manage, Conversation, :mentee_id => user.id 
       can :manage, Message, :conversation => { :mentee_id => user.id }
+      can :manage, Mentee, :id => user.id
     else
       redirect_to '/'
     end
