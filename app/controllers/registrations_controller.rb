68cc5efae269a3fd12a0afb98e5f1146aabfbe61
Jesse Pollak <jpollak92@gmail.com>
2012-07-11 13:22:16 -0400

    sign in count check

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -17,12 +17,14 @@ class RegistrationsController < Devise::RegistrationsController
       if app = App.find_by_token(params[:mentor].delete(:token))
         super
         mentor = Mentor.find_by_email(params[:mentor][:email])
-        mentor.update_attributes({ 
-          :first_name => app.name.split[0],
-          :last_name => app.name.split.slice(1, app.name.split.size).join(' '),
-          :picture_url => app.picture 
-        })
-        mentor.add_tags(app.tags.collect {|t| t.value}.uniq)
+        if mentor.sign_in_count == 1
+          mentor.update_attributes({ 
+            :first_name => app.name.split[0],
+            :last_name => app.name.split.slice(1, app.name.split.size).join(' '),
+            :picture_url => app.picture 
+          })
+          mentor.add_tags(app.tags.collect {|t| t.value}.uniq)
+        end
       else
         redirect_to '/'
       end
